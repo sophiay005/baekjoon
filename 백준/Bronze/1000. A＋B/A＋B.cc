23702c74ecfd05e2ec48a65f4ae7b244db0b0ad9
sophiay005 <88869878+sophiay005@users.noreply.github.com>
2023-05-30 20:43:38 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+int main(){
+    int inputA;
+    int inputB;
+    std::cin>>inputA;
+    std::cin>>inputB;
+    int C;
+    C = inputA + inputB;
+    std::cout<<C;
+    
+    return 0;
+}
