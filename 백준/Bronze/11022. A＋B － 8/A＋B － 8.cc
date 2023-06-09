6830d997811ca572749aa26954140c0b405788fc
sophiay005 <88869878+sophiay005@users.noreply.github.com>
2023-06-09 15:30:43 +0900

    [Bronze V] Title: A+B - 8, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    int N = 0;
+    std::cin>>N;
+    
+    for (int i=1; i<=N; i++){
+        int a,b = 0;
+        std::cin>>a>>b;
+        std::cout<<"Case #"<<i<<": "<<a<<" + "<<b<<" = "<<a+b<<std::endl;
+    }
+    
+    return 0;
+}
