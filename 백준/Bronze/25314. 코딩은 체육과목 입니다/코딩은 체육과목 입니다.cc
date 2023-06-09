7ea2f60ff6a7f44bc6864f0bd7f50a46d6b20f4e
sophiay005 <88869878+sophiay005@users.noreply.github.com>
2023-06-09 15:23:31 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    int N,l = 0;
+    std::cin>>N;
+    
+    for (int i=0; i<(N/4); i++){
+        std::cout<<"long ";
+    }
+    std::cout<<"int";
+    return 0;
+}
+
